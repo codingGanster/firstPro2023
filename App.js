2ed0165ae444e37eabcea52b5764180517b6895a
codingGanster <133964102+codingGanster@users.noreply.github.com>
2023-09-21 00:16:55 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+function greetPerson(){
+    let personName = prompt('enter your name to proceed');
+    if(personName !==''){
+        alert(personName + ' ' +'You are welcome');
+    } if( personName ===''){
+        alert('oohss Welcome Mr/Mrs no NAME');
+    }
+}
